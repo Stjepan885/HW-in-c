52c434b44dca7a83d2df332fbb993c18c2266470
Stjepan885 <turk.stipe@gmail.com>
2016-11-16 21:38:04 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+	int a;
+	int b;
+	int c=1;
+	int d;
+	int partial_quotient=0;
+	scanf_s("%d %d", &a, &b);
+	do {
+		d = b;
+		d=b*c;
+		c++;
+		partial_quotient++;
+	} while ((a-b) >= d);
+	printf("%d\n", partial_quotient);
+	system("pause");
+	return 0;
+
+}
